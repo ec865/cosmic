6a5806fb5d6e4903e8317e1309af14b3e3634930
ec865 <boraozsoy@gmail.com>
2020-05-16 19:30:48 +0300

    employee_payroll_system.txt eklendi

--- src/Evaluation.java
+++ src/Evaluation.java
@@ -2,15 +2,18 @@ import java.util.ArrayList;
 
 public class Evaluation {
     public static void main(String[] args) {
-        Parser parser = new Parser("src/input/addressbook.txt");
+        Parser parser = new Parser("src/input/employee_payroll_system.txt");
         System.out.println(parser.getInteractions());
         ArrayList<String> unique_methods = new ArrayList<>();
         for (Interaction interaction : parser.getInteractions()){
-            if (!unique_methods.contains(interaction.getMethodName()) && !interaction.getMethodName().equals("main")){
+            if (interaction.getMethodName().equals("??"))
+                unique_methods.add(interaction.getMethodName());
+            else if (!unique_methods.contains(interaction.getMethodName()) && !interaction.getMethodName().equals("main")){
                 unique_methods.add(interaction.getMethodName());
             }
         }
-        System.out.println(unique_methods);
+        for (String methodName : unique_methods)
+            System.out.println(methodName);
         System.out.println("Total CFP is "+ unique_methods.size());
     }
 }
--- src/Interaction.java
+++ src/Interaction.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 
-public class Interaction {
+public class
+Interaction {
 
     private String className;
     private ArrayList<String> classAttributes;
